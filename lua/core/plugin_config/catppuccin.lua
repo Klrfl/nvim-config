c9281edabc5a7bd12f61b6d22a4e31ac0501388e
Efraim Munthe <efrayanglain@gmail.com>
2023-09-17 21:34:13 +0700

    transparent bg

--- lua/core/plugin_config/catppuccin.lua
+++ lua/core/plugin_config/catppuccin.lua
@@ -6,7 +6,7 @@ require("catppuccin").setup({
   end,
 
   flavour = "mocha",          -- latte, frappe, macchiato, mocha
-  transparent_background = false, -- disables setting the background color.
+  transparent_background = true,
   show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
   term_colors = true,             -- sets terminal colors (e.g. `g:terminal_color_0`)
   dim_inactive = {
@@ -46,7 +46,6 @@ require("catppuccin").setup({
         background = true,
       },
     },
-    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
   },
 })
 
