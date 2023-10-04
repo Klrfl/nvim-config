9b6806500bb447744df7f50f9a8e2b3e99cf232e
Efraim Munthe <efrayanglain@gmail.com>
2023-10-04 22:14:33 +0700

    remove unecessary keybinding, move treesitter to the right

--- lua/core/plugin_config/null-ls.lua
+++ lua/core/plugin_config/null-ls.lua
@@ -1,30 +1,16 @@
 local null_ls = require("null-ls")
 
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+local group = vim.api.nvim_create_augroup("LspFormatting", { clear = false })
 local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
 
 local on_attach = function(client, bufnr)
   if client.supports_method("textDocument/formatting") then
-    vim.keymap.set("n", "<Leader>f", function()
-      vim.lsp.buf.format({
-        bufnr = bufnr,
-        async = async,
-        filter = function(c)
-          return c.name == "null-ls"
-        end
-      })
-    end, { buffer = bufnr, desc = "[lsp] format" })
-
     -- format on save
     vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
     vim.api.nvim_create_autocmd(event, {
-      buffer = bufnr,
-      group = group,
       callback = function()
         vim.lsp.buf.format({
-          bufnr = bufnr,
-          async = async,
+          async = true,
           filter = function(c)
             return c.name == "null-ls"
           end
@@ -32,12 +18,6 @@ local on_attach = function(client, bufnr)
       end,
       desc = "[lsp] format on save",
     })
-  end
-
-  if client.supports_method("textDocument/rangeFormatting") then
-    vim.keymap.set("x", "<Leader>f", function()
-      vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-    end, { buffer = bufnr, desc = "[lsp] format" })
   end
 end
 
--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,7 +1,12 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  view = {
+    side = "right",
+    width = 40
+  }
+})
 
 -- open nvimtree on startup
 -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup
