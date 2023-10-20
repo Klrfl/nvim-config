607a5677d4fceab17e97b0a94de66a3881fdaa58
Efraim Munthe <efrayanglain@gmail.com>
2023-10-20 10:42:30 +0700

    remove lightbulb sign, remove conflicting keymapping

--- lua/core/plugin_config/lspsaga.lua
+++ lua/core/plugin_config/lspsaga.lua
@@ -5,11 +5,13 @@ end
 
 saga.setup({
   server_filetype_map = {},
+  lightbulb = {
+    sign = false,
+  },
 })
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-n>", ":Lspsaga diagnostic_jump_next<CR>", opts)
-vim.keymap.set("n", "K", ":Lspsaga hover_doc<CR>", opts)
-vim.keymap.set("n", "gd", ":Lspsaga lsp_finder<CR>", opts)
+vim.keymap.set("n", "<leader>f", ":Lspsaga finder<CR>", opts)
 vim.keymap.set("n", "<C-k>", ":Lspsaga lsp_finder<CR>", opts)
 vim.keymap.set("n", "<F2>", ":Lspsaga rename<CR>", opts)
