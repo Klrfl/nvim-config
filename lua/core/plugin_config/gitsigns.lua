455d6058e6b1d90645a3c345c67dab76d9d7fd8b
Efraim Munthe <efrayanglain@gmail.com>
2023-10-21 14:21:33 +0700

    add gitsigns mappings

---
+++ lua/core/plugin_config/gitsigns.lua
@@ -0,0 +1,17 @@
+local status, gitsigns = pcall(require, "gitsigns")
+if not status then
+  return
+end
+
+gitsigns.setup({
+  on_attach = function(bufnr)
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map("n", "<leader>ghl", gitsigns.toggle_linehl)
+    map("n", "<leader>gs", gitsigns.stage_buffer)
+  end,
+})
