8660aac9ae814bd0c3e11507523989ac0a137e4a
Efraim Munthe <efrayanglain@gmail.com>
2023-11-06 15:42:15 +0700

    add condition for vscode plugin

--- init.lua
+++ init.lua
@@ -1,22 +1,25 @@
 require("klrfl.set")
 require("klrfl.keymaps")
 
--- bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
+-- load plugins if not vscode
+if not vim.g.vscode then
+  -- bootstrap lazy.nvim
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  require("lazy").setup("plugins", {
+    ui = {
+      border = "single",
+    },
   })
 end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins", {
-  ui = {
-    border = "single",
-  },
-})
