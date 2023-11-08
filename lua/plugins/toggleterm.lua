2d2b5b0588eb9dfdea5ef45f4d1899d51db96896
Efraim Munthe <efrayanglain@gmail.com>
2023-11-08 12:14:27 +0700

    add lazygit keybinding

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -18,8 +18,35 @@ return {
       vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
     end
 
+    -- toggle terminal
     vim.keymap.set("n", "<C-J>", ":ToggleTerm<CR>")
-
     vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+    -- toggle lazygit
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      direction = "float",
+      float_opts = {
+        border = "single",
+      },
+
+      -- function to run on opening the term
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
+
+      -- function to run on closing the terminal
+      on_close = function()
+        vim.cmd("startinsert!")
+      end,
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
   end,
 }
