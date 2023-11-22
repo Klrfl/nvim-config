f76a5e8eb965da6fdffcac95cf9afa796a1c5a72
Efraim Munthe <efrayanglain@gmail.com>
2023-11-22 21:37:03 +0700

    customize lualine
    add new neovim icon

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,17 +2,31 @@ return {
   "nvim-lualine/lualine.nvim",
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("lualine").setup({
-      options = {
-        icons_enabled = true,
-        theme = "catppuccin",
-      },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = "catppuccin",
+    },
 
-      sections = {
-        lualine_c = { "filename", "selectioncount" },
-        lualine_x = { "fileformat", "filetype" },
+    sections = {
+      lualine_a = {
+        {
+          "mode",
+          fmt = function(str)
+            return str .. "!!!!"
+          end,
+        },
       },
-    })
-  end,
+      lualine_c = { "filename", "selectioncount" },
+      lualine_x = { "fileformat", "filetype" },
+      lualine_z = {
+        {
+          "location",
+          fmt = function(str)
+            return str .. "| "
+          end,
+        },
+      },
+    },
+  },
 } -- status line
