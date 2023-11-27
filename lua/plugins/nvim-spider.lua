1a7e1812846ae97ac7106bfac294a3160e7bc892
Efraim Munthe <efrayanglain@gmail.com>
2023-11-27 13:27:45 +0700

    put file tree back on the right, remove modes from nvim spider

--- lua/plugins/nvim-spider.lua
+++ lua/plugins/nvim-spider.lua
@@ -1,9 +1,9 @@
 return {
   "chrisgrieser/nvim-spider",
   keys = {
-    { "w", ":lua require('spider').motion('w')<CR>", desc = "Spider-w", mode = { "n", "o", "x" } },
-    { "e", ":lua require('spider').motion('e')<CR>", desc = "Spider-e", mode = { "n", "o", "x" } },
-    { "b", ":lua require('spider').motion('b')<CR>", desc = "Spider-b", mode = { "n", "o", "x" } },
-    { "ge", ":lua require('spider').motion('ge')<CR>", desc = "Spider-ge", mode = { "n", "o", "x" } },
+    { "w", ":lua require('spider').motion('w')<CR>", desc = "Spider-w" },
+    { "e", ":lua require('spider').motion('e')<CR>", desc = "Spider-e" },
+    { "b", ":lua require('spider').motion('b')<CR>", desc = "Spider-b" },
+    { "ge", ":lua require('spider').motion('ge')<CR>", desc = "Spider-ge" },
   },
 }
