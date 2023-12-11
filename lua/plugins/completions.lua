d1d8991cb0c132e8c6922eeb80ab542632bcdada
Efraim Munthe <efrayanglain@gmail.com>
2023-12-11 21:41:59 +0700

    cmp: add keybinding for scrolling completion menu

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -18,7 +18,8 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-o>"] = cmp.mapping.complete(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
       }),
