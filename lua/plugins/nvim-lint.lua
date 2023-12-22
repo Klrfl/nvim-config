8a5f28cc88f0a526bb71587408812025780a29b3
Efraim Munthe <efrayanglain@gmail.com>
2023-12-22 12:06:09 +0700

    for eslint, lint when config file is present (or eslintConfig opt is in package.json)

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -15,7 +15,27 @@ return {
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint()
+        local function checkEslintConfigFiles()
+          local files = { ".eslintrc.cjs", ".eslintrc.json", ".eslintrc.yml", ".eslintrc.yaml" }
+
+          for _, filename in pairs(files) do
+            if vim.fn.filereadable(filename) == 1 then
+              return true
+            end
+          end
+        end
+
+        local function readPackageJson()
+          local defaults =
+            vim.fn.matchlist(vim.fn.getline(vim.fn.search("eslintConfig", "n")), "eslintConfig\\s*\\(.*\\)$")[2]
+          if defaults ~= nil then
+            return true
+          end
+        end
+
+        if checkEslintConfigFiles() or readPackageJson() then
+          lint.try_lint()
+        end
       end,
     })
   end,
