4e6d1a30bdb1bb0591af65d36f6448601bdddc1c
Efraim Munthe <efrayanglain@gmail.com>
2023-12-26 15:07:15 +0700

    dashboard: add verses

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -53,12 +53,55 @@ return {
           action = "qa",
         },
       },
-      footer = {
-        "",
-        "",
-        "Aku mengucap syukur kepada Allahku setiap kali aku mengingat kamu.",
-        "Filipi 1:3",
-      },
+      -- return random verses
+      footer = function()
+        Verses = {
+          {
+            "",
+            "",
+            "Aku mengucap syukur kepada Allahku setiap kali aku mengingat kamu.",
+            "Filipi 1:3",
+          },
+          {
+            "",
+            "",
+            "Apa pun juga yang kamu perbuat, perbuatlah dengan segenap hatimu seperti untuk Tuhan",
+            "dan bukan untuk manusia.",
+            "Kolose 3:23",
+          },
+          {
+            "",
+            "",
+            "Sebab Aku ini mengetahui rancangan-rancangan apa yang ada pada-Ku mengenai kamu,",
+            "demikianlah firman Tuhan, yaitu rancangan damai sejahtera dan bukan rancangan kecelakaan,",
+            "untuk memberikan kepadamu hari depan yang penuh harapan.",
+            "Yeremia 29:11",
+          },
+          {
+            "",
+            "",
+            "Sebab kami tidak memperhatikan yang kelihatan, melainkan yang tak kelihatan,",
+            "karena yang kelihatan adalah sementara, sedangkan yang tak kelihatan adalah kekal.",
+            "2 Korintus 4:18",
+          },
+          {
+            "",
+            "",
+            "Janganlah hatimu iri kepada orang-orang yang berdosa, tetapi takutlah akan Tuhan senantiasa. Karena",
+            "masa depan sungguh ada, dan harapanmu tidak akan hilang.",
+            "mazmur 3:17-18",
+          },
+          {
+            "",
+            "",
+            "Bersukacitalah dalam pengharapan, sabarlah dalam kesesakan, dan bertekunlah dalam doa!",
+            "Bantulah dalam kekurangan orang-orang kudus dan usahakanlah dirimu untuk selalu memberikan tumpangan!",
+            "Roma 12:12-13",
+          },
+        }
+        math.randomseed(os.time())
+        return Verses[math.random(#Verses)]
+      end,
     },
     hide = {
       statusline = false,
