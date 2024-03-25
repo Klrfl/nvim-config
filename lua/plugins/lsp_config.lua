f1f538e09c9d2fa033233e5809c4a548c8ba7135
Efraim Munthe <efrayanglain@gmail.com>
2024-03-25 19:35:07 +0700

    config vue ls for v2

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -114,10 +114,29 @@ return {
       ),
     })
 
+    local mason_registry = require("mason-registry")
+    local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+      .. "/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
+
+    -- local vue_language_server_path =
+    --   "/home/efrayanglain/.nvm/versions/node/v20.10.0/lib/node_modules/@vue/typescript-plugin"
+
+    lspconfig.tsserver.setup({
+      init_options = {
+        plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vue_language_server_path,
+            languages = { "vue" },
+          },
+        },
+      },
+      filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" },
+    })
+
     local servers = {
       "html",
       "cssls",
-      "tsserver",
       "astro",
       "volar",
       "gopls",
