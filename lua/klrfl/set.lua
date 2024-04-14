bde65b5cda5e989af0b3899afb1657ed8a477c70
Efraim Munthe <efrayanglain@gmail.com>
2024-04-14 13:41:56 +0700

    make not blinking

--- lua/klrfl/set.lua
+++ lua/klrfl/set.lua
@@ -34,10 +34,3 @@ opt.smarttab = true
 opt.autoindent = true
 opt.smartindent = true
 opt.expandtab = true
-
--- blink cursor
-vim.o.guicursor = table.concat({
-  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
-  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
-}, ",")
