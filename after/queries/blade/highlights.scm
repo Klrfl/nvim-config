e95a83b8c914bc38297bdcfd36dab84823c7f500
Efraim Munthe <efrayanglain@gmail.com>
2024-07-18 07:53:28 +0700

    update treesitter syntax for blade

--- after/queries/blade/highlights.scm
+++ after/queries/blade/highlights.scm
@@ -1,4 +1,10 @@
-(directive) @tag
-(directive_start) @tag
-(directive_end) @tag
-(comment) @comment @spell
+; https://medium.com/@jogarcia/laravel-blade-on-neovim-ee530ff5d20d
+(directive) @function
+(directive_start) @function
+(directive_end) @function
+(comment) @comment
+((parameter) @include (#set! "priority" 110)) 
+((php_only) @include (#set! "priority" 110)) 
+((bracket_start) @function (#set! "priority" 120)) 
+((bracket_end) @function (#set! "priority" 120)) 
+(keyword) @function
