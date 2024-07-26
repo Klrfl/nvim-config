dbe31ac3f2b9192e81318442c8a8a42a2fa387f2
Efraim Munthe <efrayanglain@gmail.com>
2024-07-26 22:16:21 +0700

    automatically make formatters async when slow

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,27 +2,48 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      json = { "prettierd", "prettier" },
+  config = function(bufnr)
+    local slow_format_filetypes = {}
 
-      html = { "prettierd", "prettier" },
-      css = { "prettierd", "prettier" },
-      scss = { "prettierd", "prettier" },
-      javascript = { "prettierd", "prettier" },
-      typescript = { "prettierd", "prettier" },
-      vue = { "prettierd", "prettier" },
-      astro = { "prettierd", "prettier" },
-      javascriptreact = { "prettierd", "prettier" },
-      typescriptreact = { "prettierd", "prettier" },
-      markdown = { "prettierd", "prettier" },
-      php = { "blade-formatter" },
-    },
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        json = { "prettierd", "prettier" },
 
-    format_on_save = {
-      timeout_ms = 1000,
-      lsp_fallback = true,
-    },
-  },
+        html = { "prettierd", "prettier" },
+        css = { "prettierd", "prettier" },
+        scss = { "prettierd", "prettier" },
+        javascript = { "prettierd", "prettier" },
+        typescript = { "prettierd", "prettier" },
+        vue = { "prettierd", "prettier" },
+        astro = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+        markdown = { "prettierd", "prettier" },
+        blade = { "blade-formatter" },
+      },
+
+      format_on_save = function(bufnr)
+        if slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+
+        local function on_format(err)
+          if err and err:match("timeout$") then
+            slow_format_filetypes[vim.bo[bufnr].filetype] = true
+          end
+        end
+
+        return { timeout_ms = 200, lsp_format = "fallback" }, on_format
+      end,
+
+      format_after_save = function(bufnr)
+        if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+
+        return { lsp_format = "fallback" }
+      end,
+    })
+  end,
 }
