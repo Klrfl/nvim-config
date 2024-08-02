31c484b21d3985ef50a5710c64d4853caa25c730
Efraim Munthe <efrayanglain@gmail.com>
2024-08-02 21:55:04 +0700

    turn off dim_inactive catpuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -15,11 +15,6 @@ return {
       transparent_background = true,
       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
       term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      dim_inactive = {
-        enabled = true, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.25, -- percentage of the shade to apply to the inactive window
-      },
       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         conditionals = { "altfont" },
       },
