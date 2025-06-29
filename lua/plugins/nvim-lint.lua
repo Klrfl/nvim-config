f879803208d5b221aeeb3e4b1bf05faa7754ec39
Efraim Munthe <efrayanglain@gmail.com>
2025-06-29 13:55:53 +0700

    feat: add eslint check config

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -14,12 +14,40 @@ return {
       astro = { "eslint_d" },
     }
 
+    local function eslint_config_exists()
+      local config_files = {
+        "eslint.config.js",
+        "eslint.config.mjs",
+        "eslint.config.cjs",
+        "eslint.config.ts",
+        "eslint.config.mts",
+        "eslint.config.cts",
+      }
+
+      local cwd = vim.fn.getcwd()
+
+      for _, file in ipairs(config_files) do
+        local ok, err = vim.loop.fs_stat(cwd .. "/" .. file)
+        if err then
+          return false
+        end
+
+        if ok then
+          return true
+        end
+      end
+
+      return false
+    end
+
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint()
+        if eslint_config_exists() then
+          lint.try_lint()
+        end
       end,
     })
   end,
