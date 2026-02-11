336e2f78feef1396851835c55ac78034f8d7125e
Efraim Munthe <efrayanglain@gmail.com>
2026-02-11 17:09:46 +0700

    update plugins...

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -39,18 +39,4 @@ return {
       enable = true,
     },
   },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-
-    -- blade grammar
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
-      },
-      filetype = "blade",
-    }
-  end,
 }
