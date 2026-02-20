8123f223d02a62b3343612f4c8ead1805aa86102
Efraim Munthe <efrayanglain@gmail.com>
2026-02-20 12:11:48 +0700

    use live-preview for markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,11 +1,4 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-  keys = {
-    { "<leader>mp", ":MarkdownPreviewToggle<CR>", desc = "Open Markdown Preview" },
-  },
+  "brianhuster/live-preview.nvim",
+  dependencies = { "folke/snacks.nvim" },
 }
